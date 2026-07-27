13aabde59f23e8411322a6c9e564e31ed4a25ff5
Luban78 <Luban78@gmail.com>
2026-07-27 14:59:41 +0200

    Opravena aktualizace PWA cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "programmer-journal-v2";
+const CACHE_NAME = "programmer-journal-v3";
 
 const APP_FILES = [
   "./",
@@ -6,7 +6,6 @@ const APP_FILES = [
   "./style.css",
   "./script.js",
   "./manifest.webmanifest",
-
   "./terminal.html",
   "./git.html",
   "./git-status.html",
@@ -15,7 +14,6 @@ const APP_FILES = [
   "./git-push.html",
   "./git-pull.html",
   "./git-switch.html",
-
   "./icons/icon-192.png",
   "./icons/icon-512.png",
   "./icons/icon-maskable-512.png",
@@ -27,12 +25,38 @@ self.addEventListener("install", (event) => {
       return cache.addAll(APP_FILES);
     }),
   );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) =>
+        Promise.all(
+          cacheNames
+            .filter((cacheName) => cacheName !== CACHE_NAME)
+            .map((cacheName) => caches.delete(cacheName)),
+        ),
+      ),
+  );
+
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((cachedFile) => {
-      return cachedFile || fetch(event.request);
-    }),
+    fetch(event.request)
+      .then((response) => {
+        const responseCopy = response.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseCopy);
+        });
+
+        return response;
+      })
+      .catch(() => caches.match(event.request)),
   );
 });
